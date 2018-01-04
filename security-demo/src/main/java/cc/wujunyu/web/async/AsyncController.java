25ee1227440968140bb207c0b9589b5bdded2f39
wujunyucg <junyuwu1995@foxmail.com>
2018-01-04 17:55:07 +0800

    defrredresult

--- security-demo/src/main/java/cc/wujunyu/web/async/AsyncController.java
+++ security-demo/src/main/java/cc/wujunyu/web/async/AsyncController.java
@@ -1,9 +1,12 @@
 package cc.wujunyu.web.async;
 
+import org.apache.commons.lang.RandomStringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.Callable;
 
@@ -11,18 +14,27 @@ import java.util.concurrent.Callable;
 public class AsyncController {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
+    @Autowired
+    private MockQueue mockQueue;
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
+
     @RequestMapping("/order")
-    public Callable<String> order() throws InterruptedException {
+    public DeferredResult<String> order() throws InterruptedException {
         logger.info("主线程开始");
-        Callable<String> result = new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                logger.info("副线程开始");
-                Thread.sleep(1000);
-                logger.info("副线程返回");
-                return "success";
-            }
-        };
+        String orderNumber = RandomStringUtils.randomNumeric(8);
+        mockQueue.setPlaceOrder(orderNumber);
+        DeferredResult<String> result = new DeferredResult<>();
+        deferredResultHolder.getMap().put(orderNumber, result);
+//        Callable<String> result = new Callable<String>() {
+//            @Override
+//            public String call() throws Exception {
+//                logger.info("副线程开始");
+//                Thread.sleep(1000);
+//                logger.info("副线程返回");
+//                return "success";
+//            }
+//        };
         logger.info("主线程返回");
         return result;
     }
