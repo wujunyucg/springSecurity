89cffec7bc40c0a4af766bb8f8fc55f6fd4b723d
wujunyucg <junyuwu1995@foxmail.com>
2018-01-07 20:16:49 +0800

    qq social repository

--- security-demo/src/main/java/cc/wujunyu/security/MyUserDetailService.java
+++ security-demo/src/main/java/cc/wujunyu/security/MyUserDetailService.java
@@ -1,18 +1,20 @@
-package cc.wujunyu.security.browser;
+package cc.wujunyu.security;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.social.security.SocialUser;
+import org.springframework.social.security.SocialUserDetails;
+import org.springframework.social.security.SocialUserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyUserDetailService implements UserDetailsService {
+public class MyUserDetailService implements UserDetailsService, SocialUserDetailsService {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
     @Autowired
@@ -23,7 +25,17 @@ public class MyUserDetailService implements UserDetailsService {
         logger.info("登录用户名: " + username);
         //根据用户名查找用户信息
         //根据查找到的用户信息判断用户是否冻结
-        return new User(username, passwordEncoder.encode("123456"), true, true, true, true
+        return buildUser(username);
+    }
+
+    @Override
+    public SocialUserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
+        logger.info("社交登录用户 Id: " + userId);
+        return buildUser(userId);
+    }
+
+    private SocialUserDetails buildUser(String userId) {
+        return new SocialUser(userId, passwordEncoder.encode("123456"), true, true, true, true
                 , AuthorityUtils.commaSeparatedStringToAuthorityList(
                 "admin"));
     }
