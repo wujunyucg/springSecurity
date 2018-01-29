55fdf1b5b168b0a48244731593e9acfaa5db35d4
wujunyucg <junyuwu1995@foxmail.com>
2018-01-29 20:05:08 +0800

    qq登录（默认添加新用户)

--- security-core/src/main/java/cc/wujunyu/security/core/social/SocialConfig.java
+++ security-core/src/main/java/cc/wujunyu/security/core/social/SocialConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.crypto.encrypt.Encryptors;
 import org.springframework.social.config.annotation.EnableSocial;
 import org.springframework.social.config.annotation.SocialConfigurerAdapter;
 import org.springframework.social.connect.ConnectionFactoryLocator;
+import org.springframework.social.connect.ConnectionSignUp;
 import org.springframework.social.connect.UsersConnectionRepository;
 import org.springframework.social.connect.jdbc.JdbcUsersConnectionRepository;
 import org.springframework.social.connect.web.ProviderSignInUtils;
@@ -25,10 +26,16 @@ public class SocialConfig extends SocialConfigurerAdapter {
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired(required = false)
+    private ConnectionSignUp connectionSignUp;
+
     @Override
     public UsersConnectionRepository getUsersConnectionRepository(ConnectionFactoryLocator connectionFactoryLocator) {
         JdbcUsersConnectionRepository repository = new JdbcUsersConnectionRepository(dataSource, connectionFactoryLocator, Encryptors.noOpText());
 //        repository.setTablePrefix("");
+        if(connectionSignUp != null) {
+            repository.setConnectionSignUp(connectionSignUp);
+        }
         return repository;
     }
 
---
+++ security-demo/src/main/java/cc/wujunyu/security/DemoConnectionSignUp.java
@@ -0,0 +1,14 @@
+package cc.wujunyu.security;
+
+import org.springframework.social.connect.Connection;
+import org.springframework.social.connect.ConnectionSignUp;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoConnectionSignUp implements ConnectionSignUp {
+    @Override
+    public String execute(Connection<?> connection) {
+        // 根据社交用户信息默认创建用户并返回用户唯一标识
+        return connection.getDisplayName();
+    }
+}
