1d7a85e52d0ece6d664c422ea9f979eb55bad4fb
wujunyucg <junyuwu1995@foxmail.com>
2018-02-01 18:58:24 +0800

    表单的登录，返回token

--- security-app/src/main/java/cc/wujunyu/security/app/MyResourcesServerConfig.java
+++ security-app/src/main/java/cc/wujunyu/security/app/MyResourcesServerConfig.java
@@ -1,9 +1,64 @@
 package cc.wujunyu.security.app;
 
+import cc.wujunyu.security.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import cc.wujunyu.security.core.properties.SecurityConstants;
+import cc.wujunyu.security.core.properties.SecurityProperties;
+import cc.wujunyu.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
 
 @Configuration
 @EnableResourceServer
-public class MyResourcesServerConfig {
+public class MyResourcesServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationSuccessHandler myAuthenticationSuccessHandler;
+    @Autowired
+    private AuthenticationFailureHandler myAuthenticationFailureHandler;
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer socialSecurityConfig;
+
+    @Autowired
+    private SecurityProperties securityProperties;
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+                .loginProcessingUrl(SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_FORM)
+                .successHandler(myAuthenticationSuccessHandler)
+                .failureHandler(myAuthenticationFailureHandler);
+        http//.apply(validateCodeSecurityConfig)
+//                .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(socialSecurityConfig)
+                .and()
+                .authorizeRequests()
+                .antMatchers(
+                        SecurityConstants.DEFAULT_UNAUTHENTICATION_URL,
+                        SecurityConstants.DEFAULT_LOGIN_PROCESSING_URL_MOBILE,
+                        securityProperties.getBrowser().getLoginPage(),
+                        SecurityConstants.DEFAULT_VALIDATE_CODE_URL_PREFIX+"/*",
+                        securityProperties.getBrowser().getSignUpUrl(),
+                        securityProperties.getBrowser().getSignOutUrl(),
+                        securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
+                        "/user/regist")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable();
+    }
 }
--- security-app/src/main/java/cc/wujunyu/security/app/authentication/MyAuthenticationSuccessHandler.java
+++ security-app/src/main/java/cc/wujunyu/security/app/authentication/MyAuthenticationSuccessHandler.java
@@ -1,13 +1,20 @@
 package cc.wujunyu.security.app.authentication;
 
 
-import cc.wujunyu.security.core.properties.LoginType;
 import cc.wujunyu.security.core.properties.SecurityProperties;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.collections.MapUtils;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.common.exceptions.UnapprovedClientAuthenticationException;
+import org.springframework.security.oauth2.provider.*;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -25,16 +32,67 @@ public class MyAuthenticationSuccessHandler extends SavedRequestAwareAuthenticat
     @Autowired
     private SecurityProperties securityProperties;
 
+    @Autowired
+    private ClientDetailsService clientDetailsService;
+
+    @Autowired
+    private AuthorizationServerTokenServices authorizationServerTokenServices;
+
+    private String credentialsCharset = "UTF-8";
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse
             , Authentication authentication) throws IOException, ServletException {
         logger.info("登录成功");
-        if(LoginType.JSON.equals(securityProperties.getBrowser().getLoginType())) {
-            httpServletResponse.setContentType("application/json;charset=UTF-8");
-            httpServletResponse.getWriter().write(objectMapper.writeValueAsString(authentication));
-        } else {
-            super.onAuthenticationSuccess(httpServletRequest, httpServletResponse, authentication);
+        String header = httpServletRequest.getHeader("Authorization");
+        if (header == null || !header.startsWith("Basic ")) {
+            throw new UnapprovedClientAuthenticationException("请求头中无client新");
+        }
+        String[] tokens = this.extractAndDecodeHeader(header, httpServletRequest);
+        assert tokens.length == 2;
+        String clientId = tokens[0];
+        String clientSecret = tokens[1];
+
+        ClientDetails clientDetails = clientDetailsService.loadClientByClientId(clientId);
+        if (clientDetails == null) {
+            throw new UnapprovedClientAuthenticationException("clientId对应信息不存在: " + clientId);
+        } else if (!StringUtils.equals(clientSecret, clientDetails.getClientSecret())) {
+            throw new UnapprovedClientAuthenticationException("clientSecret不匹配: " + clientId);
         }
 
+        TokenRequest tokenRequest = new TokenRequest(MapUtils.EMPTY_MAP, clientId, clientDetails.getScope(), "custom");
+
+        OAuth2Request oAuth2Request = tokenRequest.createOAuth2Request(clientDetails);
+
+        OAuth2Authentication oAuth2Authentication = new OAuth2Authentication(oAuth2Request, authentication);
+
+        OAuth2AccessToken token = authorizationServerTokenServices.createAccessToken(oAuth2Authentication);
+
+        httpServletResponse.setContentType("application/json;charset=UTF-8");
+        httpServletResponse.getWriter().write(objectMapper.writeValueAsString(token));
+
+    }
+
+    private String[] extractAndDecodeHeader(String header, HttpServletRequest request) throws IOException {
+        byte[] base64Token = header.substring(6).getBytes("UTF-8");
+
+        byte[] decoded;
+        try {
+            decoded = Base64.decode(base64Token);
+        } catch (IllegalArgumentException var7) {
+            throw new BadCredentialsException("Failed to decode basic authentication token");
+        }
+
+        String token = new String(decoded, this.getCredentialsCharset(request));
+        int delim = token.indexOf(":");
+        if (delim == -1) {
+            throw new BadCredentialsException("Invalid basic authentication token");
+        } else {
+            return new String[]{token.substring(0, delim), token.substring(delim + 1)};
+        }
+    }
+
+    protected String getCredentialsCharset(HttpServletRequest httpRequest) {
+        return this.credentialsCharset;
     }
 }
