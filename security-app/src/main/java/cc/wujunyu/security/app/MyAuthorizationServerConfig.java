2306aaea9a2dbfabaca3d92ca2d4607e7878a098
wujunyucg <junyuwu1995@foxmail.com>
2018-02-02 15:02:35 +0800

    覆盖令牌配置

---
+++ security-app/src/main/java/cc/wujunyu/security/app/MyAuthorizationServerConfig.java
@@ -0,0 +1,38 @@
+package cc.wujunyu.security.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class MyAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userDetailsService);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("wujunyu")
+                .secret("wujunyusecurity")
+                .accessTokenValiditySeconds(7200)
+                .authorizedGrantTypes("refresh_token", "password")
+                .scopes("all", "read", "write");
+
+    }
+}
