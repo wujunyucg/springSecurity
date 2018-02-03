9fde3fac89ac6e3c206f385316aaecf4a7f3d613
wujunyucg <junyuwu1995@foxmail.com>
2018-02-03 14:43:36 +0800

    配置中授权页面

--- security-browser/src/main/java/cc/wujunyu/security/browser/BrowserSecurityConfig.java
+++ security-browser/src/main/java/cc/wujunyu/security/browser/BrowserSecurityConfig.java
@@ -9,6 +9,7 @@ import cc.wujunyu.security.core.validate.code.ValidateCodeSecurityConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -103,6 +104,7 @@ public class BrowserSecurityConfig extends AbstractChannelSecurityConfig {
                         securityProperties.getBrowser().getSession().getSessionInvalidUrl(),
                         "/user/regist")
                         .permitAll()
+//                .antMatchers(HttpMethod.GET,"/user/*").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
