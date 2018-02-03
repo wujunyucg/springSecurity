5c050af03910e8ee60cf8f2416bb13adff7bd752
wujunyucg <junyuwu1995@foxmail.com>
2018-02-03 16:20:43 +0800

    security rbac service

---
+++ security-authorize/src/main/java/cc/wujunyu/security/rbac/RbacService.java
@@ -0,0 +1,10 @@
+package cc.wujunyu.security.rbac;
+
+import org.springframework.security.core.Authentication;
+
+import javax.servlet.http.HttpServletRequest;
+
+public interface RbacService {
+
+    boolean hasPermission(HttpServletRequest request, Authentication authentication);
+}
